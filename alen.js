050ed399e2f3a52ff942a91b7784b0781499c916
jayyiddd <jayyidjiddan29@gmail.com>
2024-06-06 13:55:40 +0700

    jayyid commit

--- alen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Alen = () => {
-    return (
-        <div>Alen</div>
-    )
-}
-
-export default Alen
---
+++ jayyid1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const jayyid1 = () => {
+  return (
+    <div>jayyid1</div>
+  )
+}
+
+export default jayyid1
