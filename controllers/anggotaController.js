62e371706b11ad50d014032259059734c06eabfd
jayyiddd <jayyidjiddan29@gmail.com>
2024-06-07 13:57:21 +0700

    jayyid bikin crud di anggota

--- controllers/anggotaController.js
+++ controllers/anggotaController.js
@@ -9,16 +9,85 @@ export const getAnggota = async (req, res) => {
   }
 };
 
+export const getAnggotaById = async (req, res) => {
+  try {
+    const response = await Anggota.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const updateAnggota = async (req, res) => {
+  const { nama, jenis_kelamin, alamat, telp } = req.body;
+  try {
+    const resss = await Anggota.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!resss)
+      return res.status(404).json({ msg: "Data Anggota tidak ditemukan" });
+    const response = await Anggota.update(
+      {
+        nama,
+        jenis_kelamin,
+        alamat,
+        telp,
+      },
+      {
+        where: {
+          uuid: req.params.id,
+        },
+      }
+    );
+    res.status(200).json({ msg: "Data berhasil dihapus", response });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const deleteAnggota = async (req, res) => {
+  const { nama, jenis_kelamin, alamat, telp } = req.params;
+  try {
+    const resss = await Anggota.findOne({
+      attributes: {
+        nama,
+        jenis_kelamin,
+        alamat,
+        telp,
+      },
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!resss)
+      return res.status(404).json({ msg: "Data Anggota tidak ditemukan" });
+    await Anggota.destroy({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "Data berhasil dihapus", resss });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 export const createAnggota = async (req, res) => {
   const { nama, jenis_kelamin, alamat, telp } = req.body;
   try {
-    await Peminjaman.create({
+    const response = await Anggota.create({
       nama: nama,
       jenis_kelamin: jenis_kelamin,
       alamat: alamat,
       telp: telp,
     });
-    res.status(201).json({ msg: "Pembuatan Anggota Berhasil" });
+    res.status(201).json({ msg: "Pembuatan Anggota Berhasil", response });
   } catch (error) {
     res.status(400).json({ msg: error.message });
   }
--- routes/RouteAnggota.js
+++ routes/RouteAnggota.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { createAnggota, getAnggota } from "../controllers/anggotaController.js";
+import { createAnggota, getAnggota, deleteAnggota, getAnggotaById, updateAnggota } from "../controllers/anggotaController.js";
 
 const router = express.Router();
 
 router.post("/anggota", createAnggota);
-router.get("/angoota", getAnggota);
+router.get("/anggota", getAnggota);
+router.get("/anggota/:id", getAnggotaById);
+router.delete("/anggota/:id", deleteAnggota);
+router.patch("/anggota/:id", updateAnggota);
+
+
 
 export default router;
